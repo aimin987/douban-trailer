4371b5c906544eabd1f30d58ed63bf06aafc9f43
aimin <aimin987@163.com>
2018-10-21 23:01:49 +0800

    feat:新增 HTML 静态模板文件

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
-import Koa from 'koa';
+const Koa = require('koa');
+const { normal } = require('./tpl')
 
 const app = new Koa()
 
-app.use(async (ctx, next)=> {
-    ctx.body = '电影首页'
+app.use(async (ctx, next) => {
+    ctx.type = 'text/html; charset=utf-8';
+    ctx.body = normal
 })
 
 app.listen(4455)
